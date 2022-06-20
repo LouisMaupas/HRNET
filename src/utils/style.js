7b93e5612a5c4664b42a725a3eb39e61fd302cbc
lmaupas <louis.maupas@framatome.com>
2022-06-20 16:58:32 +0200

    feat:(website) wip UI

---
+++ src/utils/style.js
@@ -0,0 +1,9 @@
+const style = {
+  primaryColor: "green",
+  primaryColorDarker: "green",
+  secondaryColor: "yellow",
+  secondaryColorLighter: "yellow",
+  activeLink: "white",
+};
+
+export default style;

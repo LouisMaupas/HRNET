5cd715bde24ca2ddf6f1c509b24d03d9a3482b64
lmaupas <louis.maupas@framatome.com>
2022-07-11 14:34:22 +0200

    refactor: remove useless

--- src/index.js
+++ src/index.js
@@ -7,13 +7,11 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <Provider store={store}>
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-  // </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
